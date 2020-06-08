93259a147c2455c21937bf6b945137c2c615b7e2
smirnovayu <smirnova.yu@gmail.com>
2020-06-08 08:36:54 +0200

    app changed 2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 import pandas as pd
-df = pd.read_csv('D:/coursera/python/Dash/voting_data_eng.csv')
+df = pd.read_csv('voting_data_eng.csv')
 # print(df.head())
 
 
@@ -103,63 +103,3 @@ if __name__ == '__main__':
     app.run_server(debug=True)
     
     
-    
-    
-    
-    
-#### first attempt
-#
-#import dash
-#import dash_core_components as dcc
-#import dash_html_components as html
-#
-#import pandas as pd
-#df = pd.read_csv('D:/coursera/python/Dash/voting_data_eng.csv')
-#
-#names = list(set(df['region_name']))
-#names.sort()
-#
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#
-#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-#
-#app.layout = html.Div([
-#        
-#    html.Div([
-#        dcc.Dropdown(
-#            id='test',
-#            options=[{'label':name, 'value':name} for name in names],
-#            value = 'Respublika Kalmykia'
-#            ),
-#            ],style={'width': '20%', 'display': 'inline-block'}),
-#                
-#  dcc.Graph(
-#        id='life-exp-vs-gdp',
-#        figure={
-#            'data': [
-#                dict(
-#                    x=df[df['region_name'] == 'Altajskij kraj']['Putin Vladimir Vladimirovich'],
-#                    y=df[df['region_name'] == 'Altajskij kraj']['Grudinin Pavel Nikolaevich'],
-#                    text=df[df['region_name'] == 'Altajskij kraj']['subregion_name'],
-#                    mode='markers',
-#                    opacity=0.7,
-#                    marker={
-#                        'size': 15,
-#                        'line': {'width': 0.5, 'color': 'white'}
-#                    }
-#                ) #for i in df.region_name.unique()
-#            ],
-#            'layout': dict(
-#                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-#                yaxis={'title': 'Life Expectancy'},
-#                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-#                legend={'x': 0, 'y': 1},
-#                hovermode='closest'
-#            )
-#        }
-#    )
-#                
-#], style={'columnCount': 1})
-#
-#if __name__ == '__main__':
-#    app.run_server(debug=True)    
